a26a1da6edee437c6bfdb6545f1d65a4d48b24f3
Anton Ahlqvist <anton.ahlqvist@iths.se>
2026-05-07 12:41:11 +0200

    Initial commit

---
+++ src/main/java/org/example/webserviceslabb1/WebServicesLabb1Application.java
@@ -0,0 +1,13 @@
+package org.example.webserviceslabb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesLabb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesLabb1Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/webserviceslabb1/WebServicesLabb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.webserviceslabb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesLabb1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
